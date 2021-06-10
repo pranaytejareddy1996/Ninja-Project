fefa81c00a357d5b492ab82bf01a1f0f778df915
pranaytejareddy1996 <60606116+pranaytejareddy1996@users.noreply.github.com>
2021-06-10 15:52:40 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,8 @@ export default function Home() {
     <div>
       <div>
       <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+      <p className={styles.text}>Hello everyone, in this website you can find some ninjas and other details. If u want to assign a ninja to assassinate your enemies please check the deails down below</p>
+      <p className={styles.text}>Click the button dow below to visit the ninjas</p>
       <Link href="/ninjas/">
         <a className={styles.btn}>See Ninja Listing</a>
       </Link>
