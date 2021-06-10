8c878c847b75cee68205e6fc1038f326bafed8d5
pranaytejareddy1996 <60606116+pranaytejareddy1996@users.noreply.github.com>
2021-06-10 16:16:22 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -2,10 +2,7 @@ import {useState} from 'react'
 import Head from 'next/head'
 
 export default function About() {
-    const [name,setName] = useState('pranay')
-    const changeName = () => {
-        setName('teja')
-    }
+    
     return (
         <>
         <Head>
@@ -14,9 +11,9 @@ export default function About() {
         </Head>
         <div>
             <h1>About Me</h1>
-            <p>Training the ninjas has been in my family from a long time. My great grand father used to train the expert ninjas and assign them to the appropriate customers. </p>
-            <p>My ancestors moved from Japan to India in 18th century.Initially for the living they created a ashram for training the ninjas. As the time goes it become an inherent profession in my family.</p>
-            <p>Now i am represnting my family and created an online website using Next Js to assgin my own ninjas whom i trained personally for the customers</p>
+            <p>Training the ninjas has been in my family for a long time. My great grand father used to train the expert ninjas and assign them to the appropriate customers. </p>
+            <p>My ancestors moved from Japan to India in 18th century.Initially for the living they created an ashram for training the ninjas. As the time progress this became an inherent profession in my family.</p>
+            <p>Now i am representing my family and created an online website using Next Js to assign my own ninjas whom i trained personally to the customers</p>
         </div>
     </>
     )
