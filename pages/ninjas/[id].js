bc3b94be73fcc653874c1f9d4024580f7b25c299
pranaytejareddy1996 <60606116+pranaytejareddy1996@users.noreply.github.com>
2021-06-10 16:11:35 +0530

    Update [id].js

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -30,10 +30,10 @@ import React from 'react'
 function Deatils({ninja}) {
     return (
         <div>
-            <h1>{ninja.name}</h1>
-            <p>{ninja.email}</p>
-            <p>{ninja.website}</p>
-            <p>{ninja.address.city}</p>
+            <h1>Name of the Ninja : {ninja.name}</h1>
+            <p>Email Id : {ninja.email}</p>
+            <p>His personal website : {ninja.website}</p>
+            <p>CUrrent location : {ninja.address.city}</p>
 
         </div>
     )
