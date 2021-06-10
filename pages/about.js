a21f870e43132848bbe049be1186a497f895c72d
pranaytejareddy1996 <60606116+pranaytejareddy1996@users.noreply.github.com>
2021-06-10 16:09:02 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -13,10 +13,10 @@ export default function About() {
             <meta name='keywords' content='ninjas'/>
         </Head>
         <div>
-            <h1>About</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+            <h1>About Me</h1>
+            <p>Training the ninjas has been in my family from a long time. My great grand father used to train the expert ninjas and assign them to the appropriate customers. </p>
+            <p>My ancestors moved from Japan to India in 18th century.Initially for the living they created a ashram for training the ninjas. As the time goes it become an inherent profession in my family.</p>
+            <p>Now i am represnting my family and created an online website using Next Js to assgin my own ninjas whom i trained personally for the customers</p>
         </div>
     </>
     )
